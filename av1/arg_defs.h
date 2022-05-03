a34afdf36729379bab32954424d5cca19c2d4da8
Chi Yo Tsai <chiyotsai@google.com>
2022-05-03 18:16:44 +0000

    ERP: Add commandline support for different pruning level

--- av1/arg_defs.h
+++ av1/arg_defs.h
@@ -140,6 +140,9 @@ typedef struct av1_codec_arg_definitions {
   arg_def_t enable_pef;
 #endif  // CONFIG_PEF
   arg_def_t disable_ml_partition_speed_features;
+#if CONFIG_EXT_RECUR_PARTITIONS
+  arg_def_t erp_pruning_level;
+#endif  // CONFIG_EXT_RECUR_PARTITIONS
   arg_def_t enable_rect_partitions;
   arg_def_t enable_ab_partitions;
   arg_def_t enable_1to4_partitions;
