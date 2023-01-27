6dbc89169209804635993479fe2bf5a4828e0467
Onur Guleryuz <oguleryuz@google.com>
2023-01-27 23:47:08 +0000

    Update aom_config_defaults.cmake

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -198,9 +198,9 @@ set_aom_config_var(CONFIG_WARP_REF_LIST 1
 # Begin: CWG-C016
 set_aom_config_var(CONFIG_LR_MERGE_COEFFS 0
                    "AV2 experiment flag to enable LR coefficient merging")
-set_aom_config_var(CONFIG_LR_FLEX_SYNTAX 0
+set_aom_config_var(CONFIG_LR_FLEX_SYNTAX 1
                    "AV2 experiment flag to enable LR flexible syntax")
-set_aom_config_var(CONFIG_WIENER_NONSEP 0 NUMBER
+set_aom_config_var(CONFIG_WIENER_NONSEP 1 NUMBER
                    "AV2 nonsep Wiener filter experiment flag")
 set_aom_config_var(CONFIG_WIENER_NONSEP_CROSS_FILT 0 NUMBER
                    "AV2 nonsep Wiener cross filter experiment flag")
