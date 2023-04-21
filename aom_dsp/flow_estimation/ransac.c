93d8d41ceec12eb453fc469d666d087d70fc45c7
Angie Chiang <angiebird@google.com>
2023-04-20 22:55:15 -0700

    Fix find_fundamental_matrix bug

--- aom_dsp/flow_estimation/ransac.c
+++ aom_dsp/flow_estimation/ransac.c
@@ -1341,7 +1341,7 @@ bool find_fundamental_matrix(int np, const double *cpts1, const double *cpts2,
   for (int r = 0; r < 3; ++r) {
     for (int c = 0; c < 3; ++c) {
       F[r * 3 + c] -=
-          U2[r * 3 + min_idx2] * V2[min_idx2 * 3 + c] * S2[min_idx2];
+          U2[r * 3 + min_idx2] * V2[c * 3 + min_idx2] * S2[min_idx2];
     }
   }
   aom_free(a);
