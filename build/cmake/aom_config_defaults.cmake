d0603196c7f1ae7cc9769b54799434973f831da2
Urvang Joshi <urvang@google.com>
2023-08-23 14:27:15 -0700

    CONFIG_FLEX_PARTITION: enable by default
    STATS_CHANGED

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -150,7 +150,7 @@ set_aom_config_var(
   CONFIG_EXT_RECUR_PARTITIONS 1 NUMBER
   "AV2 Fully recursive partitions including H partitions experiment flag")
 set_aom_config_var(CONFIG_BLOCK_256 1 NUMBER "AV2 BLOCK_256 experiment flag")
-set_aom_config_var(CONFIG_FLEX_PARTITION 0 NUMBER
+set_aom_config_var(CONFIG_FLEX_PARTITION 1 NUMBER
                    "AV2 Flexible partition experiment flag")
 set_aom_config_var(CONFIG_ERP_TFLITE 0 NUMBER "Build ERP with TFLite")
 set_aom_config_var(CONFIG_UNEVEN_4WAY 1 NUMBER
