338b8ba129e505b6e1067bfe24edeba8c58acfc1
Han Gao <gaohan@tencent.com>
2023-11-22 12:28:51 -0800

    test push

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -352,7 +352,7 @@ set_aom_config_var(CONFIG_IST_ANY_SET 1
                    "Enable R-D Optimized IST set selection from CWG-D159.")
 
 set_aom_config_var(CONFIG_D143_CCSO_FM_FLAG 1
-                   "Enable CCSO frame level joint flag.")
+                   "Enable CCSO frame level joint flag .")
 
 #
 # Variables in this section control optional features of the build system.
