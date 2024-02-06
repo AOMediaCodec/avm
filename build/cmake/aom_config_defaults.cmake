803bf3bc8755b8a9bba9a8d0d322d7c891638415
Todd Nguyen <toddnguyen@google.com>
2024-02-05 20:20:36 -0800

    Add new generated matrices including qm0/1/2.
    Set default to nearest neighbor upscaling.

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -349,6 +349,9 @@ set_aom_config_var(CONFIG_AFFINE_REFINEMENT 1
                    "Decoder side affine motion refinement.")
 set_aom_config_var(CONFIG_DERIVED_MVD_SIGN 1 "Enable MVD sign derivations")
 set_aom_config_var(CONFIG_VQ_MVD_CODING 1 "Enable VQ based MVD coding")
+set_aom_config_var(CONFIG_QM_SIMPLIFY 2
+                   "1: Bilinear upsampling, 2: Nearest-neighbor upsampling")
+
 #
 # Variables in this section control optional features of the build system.
 #
