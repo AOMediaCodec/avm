58ea54523492f6e003dfe6e94ceb4d101d5dd2ad
Stan Vitvitskyy <vitvitskyy@google.com>
2024-08-05 22:41:38 +0000

    Revert "Not optimizing LD use case, for now"
    This reverts commit 967acc6bc58a3e8b3226dfd5641e533a52cad86a.

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1131,9 +1131,8 @@ static AOM_INLINE void set_erp_speed_features(AV1_COMP *cpi) {
 #endif  // CONFIG_EXT_RECUR_PARTITIONS
   sf->part_sf.prune_rect_with_ml = cpi->oxcf.part_cfg.use_ml_erp_pruning & 1;
 #if CONFIG_ML_PART_SPLIT
-  // Don't work in LD and for the screen content
-  if (cpi->oxcf.gf_cfg.lag_in_frames > 0 &&
-      !cm->features.allow_screen_content_tools) {
+  // Don't work for the screen content
+  if (!cm->features.allow_screen_content_tools) {
     sf->part_sf.prune_split_with_ml =
         !!(cpi->oxcf.part_cfg.use_ml_erp_pruning & 2);
   }
