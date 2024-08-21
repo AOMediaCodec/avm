094eac47c0a393b94cc63130ce9428c5b22c2018
Xiaoqing Zhu <xzhu@netflix.com>
2024-08-21 17:08:38 -0500

    Fix style errors.

--- apps/aomdec.c
+++ apps/aomdec.c
@@ -537,12 +537,12 @@ static int main_loop(int argc, const char **argv_) {
   int opt_i420 = 0;
   int opt_raw = 0;
 
-  aom_codec_dec_cfg_t cfg = { 0, 0, 0
 #if CONFIG_TXFMBLK_LOGS || CONFIG_COEFF_LOGS
-                              ,
-                              NULL
+  aom_codec_dec_cfg_t cfg = { 0, 0, 0, NULL };
+#else
+  aom_codec_dec_cfg_t cfg = { 0, 0, 0 };
 #endif  // CONFIG_TXFMBLK_LOGS || CONFIG_COEFF_LOGS
-  };
+
   unsigned int fixed_output_bit_depth = 0;
   unsigned int is_annexb = 0;
   int frames_corrupted = 0;
