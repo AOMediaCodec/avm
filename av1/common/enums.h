eae381ec0c043c88d983f3b7324988521dfd9262
Han Gao <han_gao4@apple.com>
2024-11-22 10:35:44 -0800

    wedge simplification

--- av1/common/enums.h
+++ av1/common/enums.h
@@ -31,6 +31,9 @@ extern "C" {
 #define BAWP_BUGFIX 1
 #define ADJUST_SUPER_RES_Q 1
 
+#define WEDGE_2N_COS 1  // 2n cosine table
+#define WEDGE_BIT 4     // weidghting bits
+
 #if CONFIG_SUBBLK_REF_EXT
 #define SUBBLK_REF_EXT_LINES 2
 #endif  // CONFIG_SUBBLK_REF_EXT
