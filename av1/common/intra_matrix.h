9cd9b07f36c6e5a2cfeaba90da80b42c6a6c884f
Joe Young <joeyoung@google.com>
2025-02-17 19:51:46 -0800

    Added SIMD code for intra_matrix (DIP) prediction.

--- av1/common/intra_matrix.h
+++ av1/common/intra_matrix.h
@@ -9,5 +9,14 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
+#define ROWS 64
+#define COLS 16
+#define BITS 12
+#define OFFSET (1 << (12 - 1))
+#define SCALE 4
+#define FEATURES 12
+
+extern const uint16_t av1_intra_matrix_weights[][ROWS][COLS];
+
 void av1_intra_matrix_pred(const uint16_t *input, int mode, uint16_t *output,
                            int bd);
