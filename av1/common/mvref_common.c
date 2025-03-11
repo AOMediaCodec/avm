3b76502ff6b99a6a0ce5c255f95d5cb4103ebd9f
Yeqing Wu <yeqing_wu@apple.com>
2025-03-11 13:21:26 -0700

    Fix issue#542: Use of uninitialized field when order hints or allow_ref_frame_mvs disabled

--- av1/common/mvref_common.c
+++ av1/common/mvref_common.c
@@ -2569,6 +2569,10 @@ static AOM_INLINE void add_tmvp_candidate(
 // adjacent SMVP candidates based on some predefined conditions
 static AOM_INLINE int assign_tmvp_high_priority(const AV1_COMMON *cm,
                                                 MV_REFERENCE_FRAME rf[2]) {
+  if (cm->features.allow_ref_frame_mvs == 0 ||
+      cm->seq_params.order_hint_info.enable_order_hint == 0)
+    return 0;
+
   if (cm->seq_params.enable_drl_reorder == DRL_REORDER_ALWAYS) return 0;
 
   if (rf[1] == NONE_FRAME && is_inter_ref_frame(rf[0]) &&
