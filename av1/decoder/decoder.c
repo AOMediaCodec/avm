f473dea8364aa30edb0040b2dac9cdbea6d14c31
Seethal Paluri <s_paluri@apple.com>
2025-08-14 11:21:20 -0700

    formatted code. STATS_CHANGED.

--- av1/decoder/decoder.c
+++ av1/decoder/decoder.c
@@ -764,17 +764,18 @@ static void update_frame_buffers(AV1Decoder *pbi, int frame_decoded) {
       decrease_ref_count(cm->cur_frame, pool);
     } else if ((
 #if CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
-       false
+                   false
 #else
-      !cm->seq_params.order_hint_info.enable_order_hint
+                   !cm->seq_params.order_hint_info.enable_order_hint
 #endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
-      &&
+                   &&
 #if CONFIG_F253_REMOVE_OUTPUTFLAG
-        false
+                   false
 #else
-        !cm->seq_params.enable_frame_output_order
+                   !cm->seq_params.enable_frame_output_order
 #endif  // CONFIG_F253_REMOVE_OUTPUTFLAG
-         ) &&(cm->show_existing_frame || cm->show_frame)) {
+                   ) &&
+               (cm->show_existing_frame || cm->show_frame)) {
       if (pbi->output_all_layers) {
         // Append this frame to the output queue
 #if CONFIG_NEW_OBU_HEADER
@@ -951,34 +952,40 @@ int av1_get_raw_frame(AV1Decoder *pbi, size_t index, YV12_BUFFER_CONFIG **sd,
 // TODO(rachelbarker): What should this do?
 int av1_get_frame_to_show(AV1Decoder *pbi, YV12_BUFFER_CONFIG *frame) {
   if (pbi->num_output_frames == 0) return -1;
-  const size_t out_frame_idx =
-  (
+  const size_t out_frame_idx = (
 #if CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
-  false
+                                   false
 #elif CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && !CONFIG_F253_REMOVE_OUTPUTFLAG
-  false && pbi->common.seq_params.enable_frame_output_order
+                                   false && pbi->common.seq_params
+                                                .enable_frame_output_order
 #elif !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
-  false && pbi->common.seq_params.order_hint_info.enable_order_hint
+                                   false &&
+                                   pbi->common.seq_params.order_hint_info
+                                       .enable_order_hint
 #else
-   pbi->common.seq_params.order_hint_info.enable_order_hint &&
-   pbi->common.seq_params.enable_frame_output_order
-#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
-       )
-          ? pbi->output_frames_offset
-          : pbi->num_output_frames - 1;
+                                   pbi->common.seq_params.order_hint_info
+                                       .enable_order_hint &&
+                                   pbi->common.seq_params
+                                       .enable_frame_output_order
+#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT &&
+        // CONFIG_F253_REMOVE_OUTPUTFLAG
+                                   )
+                                   ? pbi->output_frames_offset
+                                   : pbi->num_output_frames - 1;
 
   if (
 #if CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
-  false
+      false
 #elif CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && !CONFIG_F253_REMOVE_OUTPUTFLAG
-  false && pbi->common.seq_params.enable_frame_output_order
+      false && pbi->common.seq_params.enable_frame_output_order
 #elif !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
-  false && pbi->common.seq_params.order_hint_info.enable_order_hint
+      false && pbi->common.seq_params.order_hint_info.enable_order_hint
 #else
-   pbi->common.seq_params.order_hint_info.enable_order_hint &&
-   pbi->common.seq_params.enable_frame_output_order
-#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
-      ) {
+      pbi->common.seq_params.order_hint_info.enable_order_hint &&
+      pbi->common.seq_params.enable_frame_output_order
+#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT &&
+        // CONFIG_F253_REMOVE_OUTPUTFLAG
+  ) {
     if (pbi->num_output_frames <= out_frame_idx) return -1;
   }
   *frame = pbi->output_frames[out_frame_idx]->buf;
