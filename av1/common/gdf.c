fdac65d99a72f99327c49a69df175337f09c0969
Debargha Mukherjee <debargha@google.com>
2025-08-15 19:09:26 +0000

    Fix GDF frame end conditions
    Fixes issue #829
    
    STATS_CHANGED

--- av1/common/gdf.c
+++ av1/common/gdf.c
@@ -433,12 +433,8 @@ void gdf_filter_frame(AV1_COMMON *cm) {
 #endif
     for (int x_pos = 0; x_pos < rec_width;
          x_pos += cm->gdf_info.gdf_block_size) {
-      for (int v_pos = y_pos; v_pos < y_pos + cm->gdf_info.gdf_block_size &&
-                              v_pos < (rec_height
-#if CONFIG_GDF_IMPROVEMENT
-                                       - GDF_TEST_STRIPE_OFF
-#endif
-                                      );
+      for (int v_pos = y_pos;
+           v_pos < y_pos + cm->gdf_info.gdf_block_size && v_pos < rec_height;
            v_pos += cm->gdf_info.gdf_unit_size) {
         int i_min = AOMMAX(v_pos, GDF_TEST_FRAME_BOUNDARY_SIZE);
         int i_max = AOMMIN(v_pos + cm->gdf_info.gdf_unit_size,
