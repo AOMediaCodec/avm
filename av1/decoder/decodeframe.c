3a8049927c60563950af7135d1ef040b0c589435
Yeqing Wu <yeqing_wu@apple.com>
2025-10-02 14:48:44 -0700

    Remove compiler flag CONFIG_REDUCED_REF_FRAME_MVS_MODE

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -6936,11 +6936,9 @@ void av1_read_sequence_header(
 #else
         seq_params->order_hint_info.enable_order_hint ? aom_rb_read_bit(rb) : 0;
 #endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
-#if CONFIG_REDUCED_REF_FRAME_MVS_MODE
     seq_params->order_hint_info.reduced_ref_frame_mvs_mode =
         seq_params->order_hint_info.enable_ref_frame_mvs ? aom_rb_read_bit(rb)
                                                          : 0;
-#endif  // CONFIG_REDUCED_REF_FRAME_MVS_MODE
 
     if (aom_rb_read_bit(rb)) {
       seq_params->force_screen_content_tools =
