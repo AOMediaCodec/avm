646dc0198fbea1d39b03f1fe408f6cd2ec6411b4
bdchoi <bdchoi@amazon.com>
2025-10-07 16:18:19 -0700

    F364: signaling background color at ats_multistream_atlas_info

--- av1/common/av1_common_int.h
+++ av1/common/av1_common_int.h
@@ -802,6 +802,12 @@ typedef struct AtlasBasicInfo {
   int ats_segment_top_left_pos_y[MAX_NUM_XLAYERS][8][8];
   int ats_segment_width[MAX_NUM_XLAYERS][8][8];
   int ats_segment_height[MAX_NUM_XLAYERS][8][8];
+#if CONFIG_BACKGRND_COLOR_ATLAS
+  int ats_default_background_present_flag[MAX_NUM_XLAYERS][8];
+  int ats_background_red_value[MAX_NUM_XLAYERS][8];
+  int ats_background_green_value[MAX_NUM_XLAYERS][8];
+  int ats_background_blue_value[MAX_NUM_XLAYERS][8];
+#endif  // CONFIG_BACKGRND_COLOR_ATLAS
 } AtlasBasicInfo;
 
 typedef struct AtlasSegmentInfo {
