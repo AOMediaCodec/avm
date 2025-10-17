526ea2032dc5672852c8b0e67d0f50b6f4fa5fce
Jungsun Kim <jungsun_kim@apple.com>
2025-10-17 06:58:22 +0200

    issue980: fix for decoding_unit_token to recognizes non-tilegroup VCL obus

--- common/obudec.c
+++ common/obudec.c
@@ -119,8 +119,30 @@ static int peek_obu_from_file(FILE *f, size_t obu_header_size, uint8_t *buffer,
   }
 #if CONFIG_F160_TD && CONFIG_F106_OBU_TILEGROUP
   if (obu_header->type == OBU_TILE_GROUP) {
+#if CONFIG_OPT_TD_BUGFIX
+    int actual_obu_header_size = 1;
+    if (obu_header->obu_extension_flag) {
+      actual_obu_header_size = 2;
+    }
+    *first_tile_group = buffer[actual_obu_header_size];
+#else
     *first_tile_group = buffer[1];
-  } else {
+#endif  // CONFIG_OPT_TD_BUGFIX
+  }
+#if CONFIG_OPT_TD_BUGFIX
+  else if (obu_header->type == OBU_TIP || obu_header->type == OBU_SEF ||
+           obu_header->type == OBU_SWITCH
+#if CONFIG_RANDOM_ACCESS_SWITCH_FRAME
+           || obu_header->type == OBU_RAS_FRAME
+#endif  // CONFIG_RANDOM_ACCESS_SWITCH_FRAME
+#if CONFIG_CWG_F317
+           || obu_header->type == OBU_BRIDGE_FRAME
+#endif  // CONFIG_CWG_F317
+  ) {
+    *first_tile_group = 1;
+  }
+#endif  // CONFIG_OPT_TD_BUGFIX
+  else {
     *first_tile_group = 0;
   }
 #endif
@@ -247,10 +269,29 @@ int obudec_read_temporal_unit(struct ObuDecInputContext *obu_ctx,
         (obu_header.type == OBU_TEMPORAL_DELIMITER && first_td != 1);
     if (!obu_ctx->has_temporal_delimiter) {
 #if CONFIG_F106_OBU_TILEGROUP
+#if CONFIG_OPT_TD_BUGFIX
+      int first_tile_group_in_frame = obu_header.type == OBU_TILE_GROUP
+                                          ? ((first_tile_group_byte >> 7) & 1u)
+                                          : first_tile_group_byte;
+#else
       int first_tile_group_in_frame = (first_tile_group_byte >> 7);
-#endif
+#endif  // CONFIG_OPT_TD_BUGFIX
+#endif  // CONFIG_F106_OBU_TILEGROUP
       decoding_unit_token =
-          ((vcl_obu_count > 0 && obu_header.type == OBU_TILE_GROUP
+          ((vcl_obu_count > 0 &&
+#if CONFIG_OPT_TD_BUGFIX
+            (obu_header.type == OBU_TILE_GROUP || obu_header.type == OBU_SEF ||
+             obu_header.type == OBU_TIP || obu_header.type == OBU_SWITCH
+#if CONFIG_RANDOM_ACCESS_SWITCH_FRAME
+             || obu_header.type == OBU_RAS_FRAME
+#endif  // CONFIG_RANDOM_ACCESS_SWITCH_FRAME
+#if CONFIG_CWG_F317
+             || obu_header.type == OBU_BRIDGE_FRAME
+#endif  // CONFIG_CWG_F317
+             )
+#else
+            obu_header.type == OBU_TILE_GROUP
+#endif  // CONFIG_OPT_TD_BUGFIX
 #if CONFIG_F106_OBU_TILEGROUP
             && first_tile_group_in_frame
 #endif
@@ -271,7 +312,14 @@ int obudec_read_temporal_unit(struct ObuDecInputContext *obu_ctx,
 #if CONFIG_F160_TD
 #if CONFIG_F106_OBU_TILEGROUP
       if (obu_header.type == OBU_TILE_GROUP || obu_header.type == OBU_SEF ||
-          obu_header.type == OBU_TIP || obu_header.type == OBU_SWITCH)
+          obu_header.type == OBU_TIP || obu_header.type == OBU_SWITCH
+#if CONFIG_RANDOM_ACCESS_SWITCH_FRAME
+          || obu_header.type == OBU_RAS_FRAME
+#endif  // CONFIG_RANDOM_ACCESS_SWITCH_FRAME
+#if CONFIG_CWG_F317
+          || obu_header.type == OBU_BRIDGE_FRAME
+#endif  // CONFIG_CWG_F317
+      )
 #else
       if (obu_header.type == OBU_FRAME || obu_header.type == OBU_FRAME_HEADER ||
           obu_header.type == OBU_REDUNDANT_FRAME_HEADER)
