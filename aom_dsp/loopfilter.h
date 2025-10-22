8ebf3a7e962ab3b069819d10f5468706bf3e17be
Andrey Norkin <anorkin@netflix.com>
2025-10-21 18:01:41 -0700

    remove CONFIG_ASYM_DF from loopfilter.

--- aom_dsp/loopfilter.h
+++ aom_dsp/loopfilter.h
@@ -50,21 +50,11 @@ static int q_first[DBL_REG_DECIS_LEN] = { 45, 43, 40, 35, 32, 32, 32, 32, 32 };
 #endif  // DF_FILT26
 
 // Determining number of samples to be modified for the current row/column
-static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
-#if CONFIG_ASYM_DF
-                                     int max_filt_neg, int max_filt_pos,
-#else
-                                     int max_filt,
-#endif  // CONFIG_ASYM_DF
-                                     uint16_t q_thresh, uint16_t side_thresh
-#if CONFIG_ASYM_DF
-                                     ,
-                                     uint16_t *t
-#endif  // CONFIG_ASYM_DF
-) {
+static INLINE int filt_choice_highbd(uint16_t *s, int pitch, int max_filt_neg,
+                                     int max_filt_pos, uint16_t q_thresh,
+                                     uint16_t side_thresh, uint16_t *t) {
   if (!q_thresh || !side_thresh) return 0;
 
-#if CONFIG_ASYM_DF
   int max_samples_neg =
 #if CONFIG_DISABLE_LOOP_FILTERS_LOSSLESS
       max_filt_neg == 0 ? 0 :
@@ -73,10 +63,6 @@ static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
   int max_samples_pos = max_filt_pos / 2 - 1;
 
   if (max_samples_pos < 1 || max_samples_pos < max_samples_neg) return 0;
-#else
-  int max_samples = max_filt / 2 - 1;
-  if (max_samples < 1) return 0;
-#endif  // CONFIG_ASYM_DF
 
   int16_t second_derivs_buf[SEC_DERIV_ARRAY_LEN];
   int16_t *second_deriv = &second_derivs_buf[(SEC_DERIV_ARRAY_LEN >> 1)];
@@ -87,24 +73,20 @@ static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
   //-----------------------------------------------
   second_deriv[-2] = abs(s[-3 * pitch] - (s[-2 * pitch] << 1) + s[-pitch]);
   second_deriv[1] = abs(s[0] - (s[pitch] << 1) + s[2 * pitch]);
-#if CONFIG_ASYM_DF
+
   second_deriv[-2] += abs(t[-3 * pitch] - (t[-2 * pitch] << 1) + t[-pitch]);
   second_deriv[-2] = (second_deriv[-2] + 1) >> 1;
 
   second_deriv[1] += abs(t[0] - (t[pitch] << 1) + t[2 * pitch]);
   second_deriv[1] = (second_deriv[1] + 1) >> 1;
-#endif  // CONFIG_ASYM_DF
 
   mask |= (second_deriv[-2] > side_thresh) * -1;
   mask |= (second_deriv[1] > side_thresh) * -1;
 
   if (mask) return 0;
 
-#if CONFIG_ASYM_DF
   if (max_samples_pos == 1) return 1;
-#else
-  if (max_samples == 1) return 1;
-#endif  // CONFIG_ASYM_DF
+
   // Testing for 2 sample modification
   //-----------------------------------------------
   const int side_thresh2 = side_thresh >> 2;
@@ -113,25 +95,20 @@ static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
   mask |= (second_deriv[1] > side_thresh2) * -1;
 
   second_deriv[-1] = abs(s[-2 * pitch] - (s[-pitch] << 1) + s[0]);
-#if CONFIG_ASYM_DF
+
   second_deriv[-1] += abs(t[-2 * pitch] - (t[-pitch] << 1) + t[0]);
   second_deriv[-1] = (second_deriv[-1] + 1) >> 1;
-#endif  // CONFIG_ASYM_DF
+
   second_deriv[0] = abs(s[-1 * pitch] - (s[0] << 1) + s[pitch]);
-#if CONFIG_ASYM_DF
+
   second_deriv[0] += abs(t[-1 * pitch] - (t[0] << 1) + t[pitch]);
   second_deriv[0] = (second_deriv[0] + 1) >> 1;
-#endif  // CONFIG_ASYM_DF
 
   mask |= ((second_deriv[-1] + second_deriv[0]) > q_thresh * DF_6_THRESH) * -1;
 
   if (mask) return 1;
 
-#if CONFIG_ASYM_DF
   if (max_samples_pos == 2) return 2;
-#else
-  if (max_samples == 2) return 2;
-#endif  // CONFIG_ASYM_DF
 
   // Testing 3 sample modification
   //-----------------------------------------------
@@ -144,7 +121,6 @@ static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
 
   int end_dir_thresh = (side_thresh * 3) >> 4;
 
-#if CONFIG_ASYM_DF
   if (max_samples_neg > 2)
     mask |= (((abs((s[-1 * pitch] - s[(-3 - 1) * pitch]) -
                    3 * (s[-1 * pitch] - s[-2 * pitch])) +
@@ -157,56 +133,23 @@ static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
              abs((t[0] - t[3 * pitch]) - 3 * (t[0] - t[1 * pitch])) + 1) >>
             1) > end_dir_thresh) *
           -1;
-#else
-  mask |= (abs((s[-1 * pitch] - s[(-3 - 1) * pitch]) -
-               3 * (s[-1 * pitch] - s[-2 * pitch])) > end_dir_thresh) *
-          -1;
-  mask |= (abs((s[0] - s[3 * pitch]) - 3 * (s[0] - s[1 * pitch])) >
-           end_dir_thresh) *
-          -1;
-#endif  // CONFIG_ASYM_DF
 
   if (mask) return 2;
 
-#if CONFIG_ASYM_DF
   if (max_samples_pos == 3) return 3;
-#else
-  if (max_samples == 3) return 3;
-#endif  // CONFIG_ASYM_DF
 
-    // Testing  4 sample modification and above
-    //-----------------------------------------------
-
-#if !CONFIG_ASYM_DF
-  int p_first_deriv_scaled = second_deriv[-2] << DF_SIDE_FIRST_SHIFT;
-  int q_first_deriv_scaled = second_deriv[1] << DF_SIDE_FIRST_SHIFT;
-#endif  // !CONFIG_ASYM_DF
+  // Testing  4 sample modification and above
+  //-----------------------------------------------
 
   int transition = (second_deriv[-1] + second_deriv[0]) << DF_Q_THRESH_SHIFT;
 
   for (int dist = 4; dist < MAX_DBL_FLT_LEN + 1; dist += 2) {
-#if !CONFIG_ASYM_DF
-    second_deriv[-dist] = abs(s[(-dist - 1) * pitch] - (s[-dist * pitch] << 1) +
-                              s[(-dist + 1) * pitch]);
-
-    second_deriv[dist - 1] = abs(
-        s[(dist - 2) * pitch] - (s[(dist - 1) * pitch] << 1) + s[dist * pitch]);
-
-    int side_thresh4 = side_thresh * side_first[dist - 4];
-#endif  // !CONFIG_ASYM_DF
-
     const int q_thresh4 = q_thresh * q_first[dist - 4];
-
-#if !CONFIG_ASYM_DF
-    mask |= (p_first_deriv_scaled > side_thresh4) * -1;
-    mask |= (q_first_deriv_scaled > side_thresh4) * -1;
-#endif  // CONFIG_ASYM_DF
 
     mask |= (transition > q_thresh4) * -1;
 
     end_dir_thresh = (side_thresh * dist) >> 4;
 
-#if CONFIG_ASYM_DF
     if (dist == 8) dist = 7;
 
     if (max_samples_neg >= dist)
@@ -225,22 +168,9 @@ static INLINE int filt_choice_highbd(uint16_t *s, int pitch,
         -1;
 
     if (dist == 7) dist = 8;
-#else
-    mask |= (abs((s[-1 * pitch] - s[(-dist - 1) * pitch]) -
-                 dist * (s[-1 * pitch] - s[-2 * pitch])) > end_dir_thresh) *
-            -1;
-    mask |= (abs((s[0] - s[dist * pitch]) - dist * (s[0] - s[1 * pitch])) >
-             end_dir_thresh) *
-            -1;
-#endif  // CONFIG_ASYM_DF
 
-#if CONFIG_ASYM_DF
     if (mask) return dist == 4 ? dist - 1 : dist - 2;
     if (max_samples_pos <= dist) return ((dist >> 1) << 1);
-#else
-    if (mask) return dist - 2;
-    if (max_samples <= dist) return ((dist >> 1) << 1);
-#endif  // CONFIG_ASYM_DF
   }
   return MAX_DBL_FLT_LEN;
 }
