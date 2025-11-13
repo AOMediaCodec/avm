11a4c346d84d2500c6025ed034f45d3578e047a6
Jungsun Kim <jungsun_kim@apple.com>
2025-11-12 20:50:34 -0800

    bugfix for the case that the first regular frame after an OLK is OLK

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -8685,10 +8685,30 @@ static int read_uncompressed_header(AV1Decoder *pbi,
 
 #if CONFIG_F106_OBU_TILEGROUP
 #if CONFIG_F024_KEYOBU
-    if (obu_type == OBU_CLK || obu_type == OBU_OLK) {
+    if (obu_type == OBU_CLK){
       current_frame->frame_type = KEY_FRAME;
-      pbi->olk_encountered = (obu_type == OBU_OLK);
-      if (obu_type == OBU_OLK && pbi->sequence_header_changed) {
+      pbi->olk_encountered = 0;
+    } else if(obu_type == OBU_OLK) {
+      current_frame->frame_type = KEY_FRAME;
+      if(pbi->olk_encountered){
+        //the first regular frame is OLK (0-4(K)-2-1-3-8(K)...
+        lock_buffer_pool(pool);
+        int ref_flags_to_keep = 0;
+        for (int layer = 0; layer <= seq_params->max_mlayer_id; layer++) {
+          if (cm->olk_refresh_frame_flags[layer] == -1) continue;
+          ref_flags_to_keep |= cm->olk_refresh_frame_flags[layer];
+        }
+        for (int ref_index = 0; ref_index < seq_params->ref_frames; ref_index++) {
+          if (!((ref_flags_to_keep >> ref_index) & 1u)) {
+            decrease_ref_count(cm->ref_frame_map[ref_index], pool);
+            cm->ref_frame_map[ref_index] = NULL;
+            if (pbi->random_accessed) pbi->valid_for_referencing[ref_index] = 1;
+          }
+        }  // ref_index
+        unlock_buffer_pool(pool);
+      }
+      pbi->olk_encountered = 1;
+      if (pbi->sequence_header_changed) {
         aom_internal_error(&cm->error, AOM_CODEC_CORRUPT_FRAME,
                            "New sequence header starts with an OLK.");
       }
