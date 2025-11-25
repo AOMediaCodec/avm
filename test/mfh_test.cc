e2789200eabd439ed61f4f9a25b7408fae0f0049
bdchoi <bdchoi@amazon.com>
2025-11-25 14:53:43 -0800

    Fix on enable_mfh_obu_signaling_

--- test/mfh_test.cc
+++ test/mfh_test.cc
@@ -89,7 +89,8 @@ class MFHTestLarge
       encoder->Control(AOME_SET_ARNR_MAXFRAMES, 7);
       encoder->Control(AOME_SET_ARNR_STRENGTH, 5);
 #if CONFIG_MULTI_FRAME_HEADER
-      encoder->SetOption("enable-mfh-obu-signaling", enable_mfh_obu_signaling_);
+      encoder->SetOption("enable-mfh-obu-signaling",
+                         enable_mfh_obu_signaling_ ? "1" : "0");
 #endif  // CONFIG_MULTI_FRAME_HEADER
     }
   }
