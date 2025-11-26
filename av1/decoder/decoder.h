b48983776880ea6366599bf444576b108a5f464b
Jungsun Kim <jungsun_kim@apple.com>
2025-11-25 16:29:14 -0800

    bugfix for qm_list size

--- av1/decoder/decoder.h
+++ av1/decoder/decoder.h
@@ -447,12 +447,14 @@ typedef struct AV1Decoder {
 #if CONFIG_F255_QMOBU
   int total_qmobu_count;
   // qmobu_list stores the data when a qmobu is parsed
+  // 15 QM OBUs can be saved due to the qm_bit_map uniqueness constraint
   struct qm_obu qmobu_list[NUM_CUSTOM_QMS];
   // Note: qm_list stores quantiztion matrices that are used during tile group
   // decoding qm_list is filled when a sequence header is activated or is filled
   // when qmobu is signalled in a temporal unit that is not containing a
   // sequence header
-  struct quantization_matrix_set qm_list[NUM_CUSTOM_QMS];
+  // qm_list[15] is assigned to the flat matrix
+  struct quantization_matrix_set qm_list[NUM_QM_LEVELS];
 #endif  // CONFIG_F255_QMOBU
 
 #if CONFIG_F024_KEYOBU
