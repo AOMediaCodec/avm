905368561432c3d503715d9bec353302287cd308
Jungsun Kim <jungsun_kim@apple.com>
2025-12-05 06:27:48 -0800

    add OBU_RAS_FRAME, OBU_SWITCH and OBU_BRIDGE_FRAME in is_regular_non_olk_obu() and set their is_leading_picture 0

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -8474,6 +8474,9 @@ static void reset_buffer_other_than_OLK(AV1Decoder *pbi) {
 }
 static int is_regular_non_olk_obu(OBU_TYPE obu_type) {
   return obu_type == OBU_REGULAR_SEF || obu_type == OBU_REGULAR_TIP ||
+         obu_type == OBU_SWITCH || obu_type == OBU_RAS_FRAME ||
+         obu_type ==
+             OBU_BRIDGE_FRAME ||  // TODO: check if BRDIGE is always regular
          obu_type == OBU_REGULAR_TILE_GROUP;
 }
 #endif
@@ -9269,8 +9272,7 @@ static int read_uncompressed_header(AV1Decoder *pbi, OBU_TYPE obu_type,
   if (!seq_params->single_picture_header_flag) {
 #if CONFIG_F024_KEYOBU
     if (pbi->olk_encountered &&
-        (is_regular_non_olk_obu(obu_type) || obu_type == OBU_BRIDGE_FRAME ||
-         obu_type == OBU_SWITCH)) {
+        is_regular_non_olk_obu(obu_type)) {
       pbi->olk_encountered = 0;
       lock_buffer_pool(pool);
       reset_buffer_other_than_OLK(pbi);
