3bdc57bded8f54553cfad55e4f1fde55e43248aa
Wan-Teh Chang <wtc@google.com>
2025-12-09 13:43:31 -0800

    Set clip_to_restricted_range to 1 for studio range
    Set the film grain parameter clip_to_restricted_range correctly: 0 for
    full range and 1 for studio range.
    
    Fixes https://gitlab.com/AOMediaCodec/avm/-/issues/1173.

--- av2/encoder/encoder_utils.c
+++ av2/encoder/encoder_utils.c
@@ -505,13 +505,12 @@ void av2_update_film_grain_parameters(struct AV2_COMP *cpi,
       if (oxcf->tool_cfg.enable_monochrome)
         reset_film_grain_chroma_params(&cm->film_grain_params);
       cm->film_grain_params.bit_depth = cm->seq_params.bit_depth;
+      cm->film_grain_params.clip_to_restricted_range =
 #if CONFIG_CWG_F270_CI_OBU
-      if (cm->ci_params.color_info.full_range_flag == AVM_CR_FULL_RANGE) {
+          cm->ci_params.color_info.full_range_flag == AVM_CR_STUDIO_RANGE;
 #else
-      if (cm->seq_params.color_range == AVM_CR_FULL_RANGE) {
+          cm->seq_params.color_range == AVM_CR_STUDIO_RANGE;
 #endif  // CONFIG_CWG_F270_CI_OBU
-        cm->film_grain_params.clip_to_restricted_range = 0;
-      }
 #if CONFIG_FGS_IDENT
 #if CONFIG_CWG_F270_CI_OBU
       if (cm->ci_params.color_info.matrix_coefficients == AVM_CICP_MC_IDENTITY)
