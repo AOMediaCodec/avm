af5583bac03d739fc36eb3357673d9e9cd598694
Jungsun Kim <jungsun_kim@apple.com>
2025-12-11 18:46:39 -0500

    update style

--- av2/av2.cmake
+++ av2/av2.cmake
@@ -129,8 +129,7 @@ list(APPEND AVM_AV2_ENCODER_SOURCES "${AVM_ROOT}/av2/encoder/pickccso.c"
 list(APPEND AVM_AV2_COMMON_SOURCES "${AVM_ROOT}/av2/common/bru.c"
      "${AVM_ROOT}/av2/common/bru.h")
 
-list(APPEND AVM_AV2_COMMON_SOURCES
-     "${AVM_ROOT}/av2/common/banding_metadata.c"
+list(APPEND AVM_AV2_COMMON_SOURCES "${AVM_ROOT}/av2/common/banding_metadata.c"
      "${AVM_ROOT}/av2/common/banding_metadata.h")
 
 list(APPEND AVM_AV2_ENCODER_SOURCES "${AVM_ROOT}/av2/encoder/trellis_quant.c"
