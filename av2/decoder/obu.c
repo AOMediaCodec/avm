1dc489bde79eb93d69844353d405f54c92d8d73c
Jungsun Kim <jungsun_kim@apple.com>
2026-01-22 11:38:42 -0800

    remove a requirement on color format in frame hash metadata

--- av2/decoder/obu.c
+++ av2/decoder/obu.c
@@ -752,13 +752,6 @@ static int read_metadata_frame_hash(AV2Decoder *const pbi,
   frame_hash->has_grain = has_grain;
   if (per_plane) {
     const int num_planes = is_monochrome ? 1 : 3;
-    // Verify that is_monochrome matches the sequence header
-    if (is_monochrome != cm->seq_params.monochrome) {
-      avm_internal_error(
-          &cm->error, AVM_CODEC_CORRUPT_FRAME,
-          "Monochrome flag mismatch in decoded frame hash metadata");
-      return -1;
-    }
     for (int i = 0; i < num_planes; ++i) {
       PlaneHash *plane = &frame_hash->plane[i];
       for (size_t j = 0; j < 16; ++j)
