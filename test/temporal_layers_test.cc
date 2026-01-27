8e8315bfdbbc97a2001b17c5f647ec44c3014445
Marco Paniconi <marpan@google.com>
2026-01-26 21:19:27 -0800

    Fixes for code review

--- test/temporal_layers_test.cc
+++ test/temporal_layers_test.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Alliance for Open Media. All rights reserved
+ * Copyright (c) 2026, Alliance for Open Media. All rights reserved
  *
  * This source code is subject to the terms of the BSD 3-Clause Clear License
  * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
@@ -28,8 +28,8 @@ class TemporalLayersTestLarge : public ::libavm_test::CodecTestWithParam<int>,
     InitializeConfig();
     passes_ = 1;
     cfg_.rc_end_usage = AVM_Q;
-    cfg_.rc_min_quantizer = 100;
-    cfg_.rc_max_quantizer = 100;
+    cfg_.rc_min_quantizer = 210;
+    cfg_.rc_max_quantizer = 210;
     cfg_.g_threads = 2;
     cfg_.g_lag_in_frames = 0;
     cfg_.g_profile = 0;
@@ -104,7 +104,7 @@ class TemporalLayersTestLarge : public ::libavm_test::CodecTestWithParam<int>,
 };
 
 TEST_P(TemporalLayersTestLarge, TemporalLayersTest2TL) {
-  ::libavm_test::Y4mVideoSource video_nonsc("paris_352_288_30.y4m", 0, 30);
+  ::libavm_test::Y4mVideoSource video_nonsc("park_joy_90p_8_420.y4m", 0, 20);
   num_temporal_layers_ = 2;
   decode_base_only_ = false;
   drop_tl2_ = false;
@@ -112,7 +112,7 @@ TEST_P(TemporalLayersTestLarge, TemporalLayersTest2TL) {
 }
 
 TEST_P(TemporalLayersTestLarge, TemporalLayersTestDecodeBaseOnly2TL) {
-  ::libavm_test::Y4mVideoSource video_nonsc("paris_352_288_30.y4m", 0, 30);
+  ::libavm_test::Y4mVideoSource video_nonsc("park_joy_90p_8_420.y4m", 0, 20);
   num_temporal_layers_ = 2;
   decode_base_only_ = true;
   drop_tl2_ = false;
@@ -120,7 +120,7 @@ TEST_P(TemporalLayersTestLarge, TemporalLayersTestDecodeBaseOnly2TL) {
 }
 
 TEST_P(TemporalLayersTestLarge, TemporalLayersTest3TL) {
-  ::libavm_test::Y4mVideoSource video_nonsc("paris_352_288_30.y4m", 0, 30);
+  ::libavm_test::Y4mVideoSource video_nonsc("park_joy_90p_8_420.y4m", 0, 20);
   num_temporal_layers_ = 3;
   decode_base_only_ = false;
   drop_tl2_ = false;
@@ -128,7 +128,7 @@ TEST_P(TemporalLayersTestLarge, TemporalLayersTest3TL) {
 }
 
 TEST_P(TemporalLayersTestLarge, TemporalLayersTestDecodeBaseOnly3TL) {
-  ::libavm_test::Y4mVideoSource video_nonsc("paris_352_288_30.y4m", 0, 30);
+  ::libavm_test::Y4mVideoSource video_nonsc("park_joy_90p_8_420.y4m", 0, 20);
   num_temporal_layers_ = 3;
   decode_base_only_ = true;
   drop_tl2_ = false;
@@ -136,12 +136,12 @@ TEST_P(TemporalLayersTestLarge, TemporalLayersTestDecodeBaseOnly3TL) {
 }
 
 TEST_P(TemporalLayersTestLarge, TemporalLayersTestDropTopEnh3TL) {
-  ::libavm_test::Y4mVideoSource video_nonsc("paris_352_288_30.y4m", 0, 30);
+  ::libavm_test::Y4mVideoSource video_nonsc("park_joy_90p_8_420.y4m", 0, 20);
   num_temporal_layers_ = 3;
   decode_base_only_ = false;
   drop_tl2_ = true;
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video_nonsc));
 }
 
-AV2_INSTANTIATE_TEST_SUITE(TemporalLayersTestLarge, ::testing::Values(4));
+AV2_INSTANTIATE_TEST_SUITE(TemporalLayersTestLarge, ::testing::Values(5));
 }  // namespace
