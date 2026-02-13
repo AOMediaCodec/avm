wjdz99/aom

Convert EncoderTest::last_pts_ to a local variable
Convert the data member EncoderTest::last_pts_ to a local variable in
the EncoderTest::RunLoop() method. EncoderTest::last_pts_ is only used
in that method, and that method first sets EncoderTest::last_pts_ to 0
before using it. So EncoderTest::last_pts_ is effectively a local
variable in that method.

Note that DatarateTest, a subclass of EncoderTest, declares its own
last_pts_ data member and uses it to calculate the data rate. Apparently
DatarateTest::last_pts_ hides the same-named data member in the base
class EncoderTest. Although this is allowed by C++, this is very
confusing.

Ref: https://chromium-review.googlesource.com/c/webm/libvpx/+/3700777

Change-Id: I159c42e66ce6724dbdba1507d1ecbf023f9d06c3

test/encode_test_driver.cc
@@ -177,7 +177,7 @@ void EncoderTest::RunLoop(VideoSource *video) {
 
   ASSERT_TRUE(passes_ == 1 || passes_ == 2);
   for (unsigned int pass = 0; pass < passes_; pass++) {
-    last_pts_ = 0;
+    aom_codec_pts_t last_pts = 0;
 
     if (passes_ == 1)
       cfg_.g_pass = AOM_RC_ONE_PASS;
@@ -255,8 +255,8 @@ void EncoderTest::RunLoop(VideoSource *video) {
                 has_dxdata = true;
               }
 #endif
-              ASSERT_GE(pkt->data.frame.pts, last_pts_);
-              if (sl == number_spatial_layers_) last_pts_ = pkt->data.frame.pts;
+              ASSERT_GE(pkt->data.frame.pts, last_pts);
+              if (sl == number_spatial_layers_) last_pts = pkt->data.frame.pts;
               FramePktHook(pkt);
               break;
 

test/encode_test_driver.h
@@ -194,7 +194,7 @@ class EncoderTest {
  protected:
   explicit EncoderTest(const CodecFactory *codec)
       : codec_(codec), abort_(false), init_flags_(0), frame_flags_(0),
-        last_pts_(0), mode_(kRealTime), number_spatial_layers_(1) {
+        mode_(kRealTime), number_spatial_layers_(1) {
     // Default to 1 thread.
     cfg_.g_threads = 1;
   }
@@ -279,7 +279,6 @@ class EncoderTest {
   TwopassStatsStore stats_;
   aom_codec_flags_t init_flags_;
   unsigned long frame_flags_;
-  aom_codec_pts_t last_pts_;
   TestMode mode_;
   int number_spatial_layers_;
 };

AOMediaCodec/avm

test/encode_test_driver.cc
@@ -168,7 +168,7 @@ void EncoderTest::RunLoop(VideoSource *video) {
 
   ASSERT_EQ(1, (int)passes_);
   for (unsigned int pass = 0; pass < passes_; pass++) {
-    last_pts_ = 0;
+    avm_codec_pts_t last_pts = 0;
 
     if (passes_ == 1)
       cfg_.g_pass = AVM_RC_ONE_PASS;
@@ -218,8 +218,8 @@ void EncoderTest::RunLoop(VideoSource *video) {
                 if (!HandleDecodeResult(res_dec, decoder.get())) break;
                 has_dxdata = true;
               }
-              ASSERT_GE(pkt->data.frame.pts, last_pts_);
-              if (sl == number_spatial_layers_) last_pts_ = pkt->data.frame.pts;
+              ASSERT_GE(pkt->data.frame.pts, last_pts);
+              if (sl == number_spatial_layers_) last_pts = pkt->data.frame.pts;
               FramePktHook(pkt, &dec_iter);
               break;
             case AVM_CODEC_CX_FRAME_PKT:
@@ -240,8 +240,8 @@ void EncoderTest::RunLoop(VideoSource *video) {
                 has_dxdata = true;
                 pkt_decoded = true;
               }
-              ASSERT_GE(pkt->data.frame.pts, last_pts_);
-              if (sl == number_spatial_layers_) last_pts_ = pkt->data.frame.pts;
+              ASSERT_GE(pkt->data.frame.pts, last_pts);
+              if (sl == number_spatial_layers_) last_pts = pkt->data.frame.pts;
               FramePktHook(pkt, NULL);
               break;
 

test/encode_test_driver.h
@@ -180,7 +180,7 @@ class EncoderTest {
  protected:
   explicit EncoderTest(const CodecFactory *codec)
       : codec_(codec), abort_(false), init_flags_(0), frame_flags_(0),
-        last_pts_(0), mode_(kOnePassGood), number_spatial_layers_(1) {
+        mode_(kOnePassGood), number_spatial_layers_(1) {
     // Default to 1 thread.
     cfg_.g_threads = 1;
   }
@@ -271,7 +271,6 @@ class EncoderTest {
   unsigned int passes_;
   avm_codec_flags_t init_flags_;
   unsigned long frame_flags_;
-  avm_codec_pts_t last_pts_;
   TestMode mode_;
   int number_spatial_layers_;
 };