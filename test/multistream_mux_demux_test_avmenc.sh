a46dd58a67c0dbf7bf384cad9e0fee9f33eaff6d
Marco Paniconi <marpan@google.com>
2026-02-25 15:26:58 -0800

    split up the tests

--- test/multistream_mux_demux_test_avmenc.sh
+++ test/multistream_mux_demux_test_avmenc.sh
@@ -9,11 +9,11 @@
 # Run complete encode, mux, and demux pipeline
 run_encode_mux_demux() {
 
-  echo "Start multi layer streams"
+  echo "Start single layer stream"
 
-  echo "(#temporal, #embedded) = (1, 1)"
-  ml_encode_bitstream_0 1 1 0 10 || return 1
-  ml_encode_bitstream_1 1 1 0 10 || return 1
+  echo "avmenc with lag = 0"
+  encode_bitstream_0 || return 1
+  encode_bitstream_1 || return 1
   decode_bitstream_0 || return 1
   decode_bitstream_1 || return 1
   mux_bitstreams || return 1
@@ -22,158 +22,7 @@ run_encode_mux_demux() {
   decode_muxed_bitstream || return 1
   compare_md5 || return 1
 
-  echo "(#temporal, #embedded) = (2, 1)"
-  ml_encode_bitstream_0 2 1 0 10 || return 1
-  ml_encode_bitstream_1 2 1 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (3, 1)"
-  ml_encode_bitstream_0 3 1 0 10 || return 1
-  ml_encode_bitstream_1 3 1 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (2, 1) and (1, 1) for first/second stream"
-  ml_encode_bitstream_0 2 1 0 10 || return 1
-  ml_encode_bitstream_1 1 1 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (3, 1) and (1, 1) for first/second stream"
-  ml_encode_bitstream_0 3 1 0 10 || return 1
-  ml_encode_bitstream_1 1 1 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (3, 1) and (2, 1) for first/second stream"
-  ml_encode_bitstream_0 3 1 0 10 || return 1
-  ml_encode_bitstream_1 2 1 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (2, 1) for nonzero lag"
-  ml_encode_bitstream_0 2 1 15 20 || return 1
-  ml_encode_bitstream_1 2 1 15 20 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (1, 2)"
-  ml_encode_bitstream_0 1 2 0 10 || return 1
-  ml_encode_bitstream_1 1 2 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (2, 2)"
-  ml_encode_bitstream_0 2 2 0 10 || return 1
-  ml_encode_bitstream_1 2 2 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (1, 2) and (2, 1) for first/second stream"
-  ml_encode_bitstream_0 1 2 0 10 || return 1
-  ml_encode_bitstream_1 2 1 0 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (3, 3)"
-  ml_encode_bitstream_0 3 3 0 4|| return 1
-  ml_encode_bitstream_1 3 3 0 4 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (1, 2) for nonzero lag"
-  ml_encode_bitstream_0 1 2 15 20 || return 1
-  ml_encode_bitstream_1 1 2 15 20 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "(#temporal, #embedded) = (2, 2) for nonzero lag"
-  ml_encode_bitstream_0 2 2 15 20 || return 1
-  ml_encode_bitstream_1 2 2 15 20 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  mux_bitstreams || return 1
-  demux_bitstream || return 1
-  compare_bitstreams || return 1
-  decode_muxed_bitstream || return 1
-  compare_md5 || return 1
-
-  echo "test 4 multi layer streams:"
-  echo "1. (#temporal, #embedded) = (3, 1)"
-  echo "2. (#temporal, #embedded) = (1, 2)"
-  echo "3. (#temporal, #embedded) = (1, 3)"
-  echo "4. (#temporal, #embedded) = (2, 1) with lag"
-  ml_encode_bitstream_0 3 1 0 10 || return 1
-  ml_encode_bitstream_1 1 2 0 10 || return 1
-  ml_encode_bitstream_2 1 3 0 4 || return 1
-  ml_encode_bitstream_3 2 1 15 10 || return 1
-  decode_bitstream_0 || return 1
-  decode_bitstream_1 || return 1
-  decode_bitstream_2 || return 1
-  decode_bitstream_3 || return 1
-  mux_bitstreams4 || return 1
-  demux_bitstream4 || return 1
-  compare_bitstreams4 || return 1
-  decode_muxed_bitstream4 || return 1
-  compare_md5_4 || return 1
-
-  echo "Done with multi layer streams"
+  echo "Done avmenc single layer stream"
 }
 
 # Test list
