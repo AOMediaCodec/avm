cf3856973a09436a89ab5b871634143434242ead
Urvang Joshi <urvang@google.com>
2026-08-25 14:58:07 -0700

    Use encoder-only method for reduced-tx-part-set option (#5313)
    - Always enable all tx types at sequence level
    - Disable 4way/5way tx types only during RDO
    
    Fixes https://github.com/AOMediaCodec/av2-spec-internal/issues/1262
    using encoder-only method.

--- av2/encoder/encoder.c
+++ av2/encoder/encoder.c
@@ -577,7 +577,12 @@ void av2_init_seq_coding_tools(AV2_COMP *cpi, SequenceHeader *seq,
   seq->enable_mhccp = oxcf->intra_mode_cfg.enable_mhccp;
   seq->enable_inter_ddt =
       seq->single_picture_header_flag ? 0 : oxcf->txfm_cfg.enable_inter_ddt;
-  seq->reduced_tx_part_set = oxcf->txfm_cfg.reduced_tx_part_set;
+  // Note: we always keep the sequence header flag `reduced_tx_part_set` off,
+  // ignoring the encoder option `oxcf->txfm_cfg.reduced_tx_part_set`. This is
+  // because the restricting the transform partition types during encoder RDO
+  // only has been found to be more compression-efficient than using the
+  // sequence header option.
+  seq->reduced_tx_part_set = 0;
   seq->enable_cctx = oxcf->txfm_cfg.enable_cctx;
   seq->enable_ibp = oxcf->intra_mode_cfg.enable_ibp;
   seq->enable_adaptive_mvd =
