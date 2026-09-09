742fdbe8609848cae4d5060820cbe93dd6fe555d
Yixin Du <yixin_du@apple.com>
2026-09-09 09:33:48 -0700

    partition_sms: replace SMS MVs with TMVP MVs, retrain weights
    Update the SMS partition pre-screener to use TMVP-projected MVs instead
    of motion-search MVs, and retrain the MLP on the updated feature set.
    Also enables reduce_prec=1 to round MLP logits to 9-bit precision,
    fixing non-deterministic encode results across platforms.
    
    Anchor: b42be4ef
    Test condition: CTC, RA, 33 frames, speed 4
    
    RA:
    ```
    +------------------+-------+-------+-------+-------+-------+
    | Class            |     Y |    Cb |    Cr |  wAvg |  Enc% |
    +------------------+-------+-------+-------+-------+-------+
    | A1               |  0.20 |  0.05 |  0.14 |  0.19 |  95.4 |
    | A2               |  0.13 |  0.01 |  0.18 |  0.13 |  95.1 |
    | A3               |  0.20 | -0.47 | -0.13 |  0.16 |  95.4 |
    +------------------+-------+-------+-------+-------+-------+
    | Average w/o B2   |  0.16 | -0.05 |  0.14 |  0.15 |  96.3 |
    +------------------+-------+-------+-------+-------+-------+
    ```
    
    STATS_CHANGED

--- av2/encoder/partition_sms.h
+++ av2/encoder/partition_sms.h
@@ -10,6 +10,7 @@
 #include "av2/encoder/encoder.h"
 #include "av2/encoder/encodeframe_utils.h"
 #include "av2/encoder/partition_strategy.h"
+#include "av2/common/mv.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,6 +18,12 @@ extern "C" {
 
 /* Number of AVM partition types (NONE..SPLIT). */
 #define SMS_UNIFIED_N_CLASSES 10
+
+/* Average TMVP MVs over a rectangular MI region (mi_row, mi_col, mi_h×mi_w).
+ * Returns zero MV when TMVP is unavailable or all sampled entries are invalid.
+ */
+MV av2_get_tmvp_mv_avg(const AV2_COMMON *cm, int mi_row, int mi_col, int mi_h,
+                       int mi_w);
 
 /* Run MLP inference for the block at (mi_row, mi_col) with the given bsize.
  * Stores per-class softmax probabilities in sms_tree->sms_unified_probs[],
