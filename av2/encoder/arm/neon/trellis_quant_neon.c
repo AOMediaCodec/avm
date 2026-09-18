afe993e3aca5d7465ff37c9eb265d5672f1577c7
Joseph Justiss <jjustiss@apple.com>
2026-09-18 11:56:49 -0700

    Address review: remove identity branch, carry-forward vmin, fix test coverage

--- av2/encoder/arm/neon/trellis_quant_neon.c
+++ av2/encoder/arm/neon/trellis_quant_neon.c
@@ -33,10 +33,6 @@ void av2_update_nbr_diagonal_neon(struct tcq_ctx_t *tcq_ctx, int row, int col,
   uint8x8_t max_v = vdup_n_u8(max2);
 
   uint8x8_t orig_v = vld1_u8((const uint8_t *)tcq_ctx->orig_st);
-  uint8x8_t identity_v = vcreate_u8(0x0706050403020100ULL);
-  uint8x8_t cmp = vceq_u8(orig_v, identity_v);
-  int orig_is_identity =
-      (vget_lane_u64(vreinterpret_u64_u8(cmp), 0) == 0xFFFFFFFFFFFFFFFFULL);
 
   uint8x8_t s1 = vld1_u8((const uint8_t *)tcq_ctx->prev_st[idx0]);
 
@@ -47,13 +43,14 @@ void av2_update_nbr_diagonal_neon(struct tcq_ctx_t *tcq_ctx, int row, int col,
   uint8x8_t prev_lev1 = vld1_u8(tcq_ctx->lev_new[idx0]);
   uint8x8_t prev_lev2 = vtbl1_u8(vld1_u8(tcq_ctx->lev_new[idx0 + 1]), s1);
 
+  uint8x8_t l0m1 = vmin_u8(prev_lev1, max1_v);
+  uint8x8_t l1m1 = vmin_u8(prev_lev2, max1_v);
+  uint8x8_t l0m2 = vmin_u8(prev_lev1, max_v);
+  uint8x8_t l1m2 = vmin_u8(prev_lev2, max_v);
+
   for (int i = idx0; i < idx_end; i++) {
-    uint8x8_t mag_base_raw = vld1_u8(tcq_ctx->mag_base[i]);
-    uint8x8_t mag_mid_raw = vld1_u8(tcq_ctx->mag_mid[i]);
-    uint8x8_t old_base =
-        orig_is_identity ? mag_base_raw : vtbl1_u8(mag_base_raw, orig_v);
-    uint8x8_t old_mid =
-        orig_is_identity ? mag_mid_raw : vtbl1_u8(mag_mid_raw, orig_v);
+    uint8x8_t old_base = vtbl1_u8(vld1_u8(tcq_ctx->mag_base[i]), orig_v);
+    uint8x8_t old_mid = vtbl1_u8(vld1_u8(tcq_ctx->mag_mid[i]), orig_v);
 
     uint8x8_t lev0 = prev_lev1;
     uint8x8_t lev1 = prev_lev2;
@@ -65,8 +62,6 @@ void av2_update_nbr_diagonal_neon(struct tcq_ctx_t *tcq_ctx, int row, int col,
     prev_lev1 = lev1;
     prev_lev2 = lev2;
 
-    uint8x8_t l0m1 = vmin_u8(lev0, max1_v);
-    uint8x8_t l1m1 = vmin_u8(lev1, max1_v);
     uint8x8_t base_sum = vqadd_u8(l0m1, l1m1);
     uint8x8_t base = vmin_u8(vrhadd_u8(old_base, base_sum), base_max_v);
 
@@ -75,11 +70,14 @@ void av2_update_nbr_diagonal_neon(struct tcq_ctx_t *tcq_ctx, int row, int col,
 
     vst1_u8(tcq_ctx->ctx[i], vsli_n_u8(base, mid, 4));
 
-    uint8x8_t l0m2 = (max1 == max2) ? l0m1 : vmin_u8(lev0, max_v);
-    uint8x8_t l1m2 = (max1 == max2) ? l1m1 : vmin_u8(lev1, max_v);
     uint8x8_t l2m2 = vmin_u8(lev2, max_v);
     vst1_u8(tcq_ctx->mag_base[i], vqadd_u8(vqadd_u8(l0m2, l1m2), l2m2));
     vst1_u8(tcq_ctx->mag_mid[i], lev1);
+
+    l0m1 = l1m1;
+    l1m1 = vmin_u8(lev2, max1_v);
+    l0m2 = l1m2;
+    l1m2 = l2m2;
   }
 
   vst1_u8((uint8_t *)tcq_ctx->orig_st, vcreate_u8(0x0706050403020100ULL));
